03ec0ae61b976b230e4df3988f999b44b66aea15
schifzt <26299162+schifzt@users.noreply.github.com>
2022-02-02 00:33:28 +0900

    update repository name

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# wordle-solver-ts
-
+# a-certain-game-solver
 # build & run
 ```node
 npm run build
 npm run start
 ```
 
+# License
+Distributed under the [MIT license](https://opensource.org/licenses/MIT).
+
+The list of words comes from https://www-cs-faculty.stanford.edu/~knuth/sgb.html, which is public-domain sources.
+
+# Author
+[shifzt](https://github.com/schifzt)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
-function hello(name: string): string {
-    return `Hello, ${name}!`;
+import { readFileSync } from 'fs';
+import * as readline from 'readline';
+const Colors = require('colors');
+
+
+const mode = "play";
+
+const file: string = readFileSync('resources/sgb-words.txt', 'utf-8');
+const words: string[] = file.split("\n");
+// for (const word of words) {
+//     console.log(colors.blue(word));
+// }
+
+// type statusColors = {
+//     correct: string;
+//     justin: string;
+//     missed: string;
+// }
+
+const square_size: number = 3;
+function correct(s: string = " "): string {
+    return Colors.black(s + s).bgGreen.repeat(square_size);
 }
 
-console.log(hello("World"));
+function justin(s: string = " "): string {
+    return Colors.black(s + s).bgYellow.repeat(square_size);
+}
+
+function missed(s: string = " "): string {
+    return Colors.white(s + s).bgGrey.repeat(square_size);
+}
+const bar: string = Colors.white(" ");
+
+var out: string = correct() + bar + correct() + bar + justin() + bar + missed() + bar + missed();
+for (let i = 0; i < square_size; i++) {
+    console.log(out);
+}
+
+// main
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+rl.setPrompt('> ');
+
+// inifinte loop
+rl.prompt();
+rl.on('line', (input) => {
+    console.log(Colors.blue(input));
+    rl.prompt();
+});
